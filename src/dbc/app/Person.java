1703979c0eccd0c894bd71c781ccfe88dfe03358
Umesh Pratap Singh <53749680+thakurups@users.noreply.github.com>
2020-02-19 22:14:55 +0530

    person class and its getter and setter

--- src/dbc/app/Person.java
+++ src/dbc/app/Person.java
@@ -1,4 +1,33 @@
 package dbc.app;
 
+import java.util.ArrayList;
+
 public class Person {
+    private String firstName;
+    private String lastName;
+    private ArrayList<String> contactNumbers;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public ArrayList<String> getContactNumbers() {
+        return contactNumbers;
+    }
+
+    public void setContactNumbers(ArrayList<String> contactNumbers) {
+        this.contactNumbers = contactNumbers;
+    }
 }
